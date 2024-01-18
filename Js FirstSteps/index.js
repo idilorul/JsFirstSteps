de2d5f3a66994142f4d42fc7c4ee682dd43bc702
Eylül İdil Orul <155921285+idilorul@users.noreply.github.com>
2024-01-18 01:07:30 +0100

    Add files via upload

---
+++ Js FirstSteps/index.js
@@ -0,0 +1,15 @@
+const can = ('Can ARda');
+
+console.log(can)
+
+// Degisken isimleri olamazlar
+const SyntaxError 
+const new 
+const function name(params) {
+    
+}
+const name 
+const null 
+
+const number = (!false);
+console.log(number);
